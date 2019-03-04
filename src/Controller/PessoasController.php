17d870950068b5ad3e99c9f7c8509b0e8b79c555
Alexandre Matheus <alexandrematheuscg@hotmail.com>
2019-03-04 10:11:41 -0300

    Update PessoasController.php

--- src/Controller/PessoasController.php
+++ src/Controller/PessoasController.php
@@ -14,7 +14,8 @@
     use Symfony\Component\Form\Extension\Core\Type\SubmitType;
     use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 
-    class PessoasController extends Controller {
+    class PessoasController extends Controller 
+    {
 
         /**
          * @Route("/pessoas/listar" ,name="listarPessoa")
@@ -23,7 +24,8 @@
         public function listarPessoas()
         {
             $pessoas = $this->getDoctrine()->getRepository(Pessoas::class)->findAll();
-            return $this->render('Pessoas/indexPessoas.html.twig',array('pessoas'=>$pessoas));
+
+            return $this->render('Pessoas/indexPessoas.html.twig', array('pessoas'=>$pessoas));
         }
 
         /**
@@ -34,20 +36,60 @@
         {
             $pessoas = new Pessoas();
             $form = $this->createFormBuilder($pessoas)
-            ->add('coCpfCnpj', TextType::class, array('attr' => array('class' => 'form-control','required'=> true,'min'=>11,'max' => 14,'onblur'=>'javascript: formatarCampo(this);','onfocus'=>'javascript: retirarFormatacao(this);')))
-            ->add('nuSeqEmpresa', TextType::class, array('attr' => array('class' => 'form-control','required'=> true,'onblur'=>'javascript: formatarCampo(this);','onfocus'=>'javascript: retirarFormatacao(this);')))
-            ->add('dsNome', TextType::class, array('attr' => array('class' => 'form-control','required'=> true,'maxlength' => 100)))
-            ->add('salvar', SubmitType::class, array('label' => 'Salvar', 'attr' => array('class' => 'btn btn-primary mt-3')))
+            ->add(
+                'coCpfCnpj',
+                TextType::class,
+                [
+                    'attr' =>
+                    [
+                        'class' =>'form-control',
+                        'required'=> true,
+                        'max' => 14,
+                        'onblur'=>'javascript: formatarCampo(this);',
+                        'onfocus'=>'javascript: retirarFormatacao(this);'
+                    ]
+                ])
+            ->add(
+                'nuSeqEmpresa',
+                TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' =>'form-control',
+                            'required'=> true,
+                            'onblur'=>'javascript: formatarCampo(this);',
+                            'onfocus'=>'javascript: retirarFormatacao(this);'
+                        ]
+                ])
+            ->add(
+                'dsNome',
+                TextType::class,
+                [
+                    'attr' =>
+                    [
+                        'class' => 'form-control',
+                        'required'=> true,
+                        'maxlength' => 100
+                    ]
+                ])
+            ->add('salvar',
+                SubmitType::class,
+                [
+                    'label' => 'Salvar',
+                    'attr' => ['class' => 'btn btn-primary mt-3']
+                ])
             ->getForm();
-
             $form->handleRequest($request);
+
             if ($form->isSubmitted() && $form->isValid()) {
                 $pessoa = $form->getData();
                 $entityManager = $this->getDoctrine()->getManager();
                 $entityManager->persist($pessoa);
                 $entityManager->flush();
+
                 return $this->redirectToRoute('listarPessoa');
             }
+
             return $this->render('Pessoas/criarPessoas.html.twig', array('form' => $form->createView()));
         }
 
@@ -55,7 +97,7 @@
          * @Route("/pessoas/deletar/{nuSeqPessoa}", name="baixarPessoa")
          * @Method({"DELETE"})
          */
-        public function deletarPessoa (Request $request, $nuSeqPessoa) 
+        public function deletarPessoa(Request $request, $nuSeqPessoa)
         {
             $pessoa = $this->getDoctrine()->getRepository(Pessoas::class)->find($nuSeqPessoa);
             $entityManager = $this->getDoctrine()->getManager();
@@ -63,6 +105,7 @@
             $entityManager->flush();
             $response = new Response();
             $response->send();
+
             return $this->redirectToRoute('listarPessoa');
         }
 
@@ -70,36 +113,75 @@
         * @Route("/pessoas/editar/{nuSeqPessoa}", name="editarPessoa")
         * Method({"GET", "POST"})
         */
-        public function editarPessoa(Request $request, $nuSeqPessoa) 
+        public function editarPessoa(Request $request, $nuSeqPessoa)
         {
             $pessoa = new Pessoas();
             $pessoa = $this->getDoctrine()->getRepository(Pessoas::class)->find($nuSeqPessoa);
             $form = $this->createFormBuilder($pessoa)
-            ->add('nuSeqEmpresa', NumberType::class, array('attr' => array('class' => 'form-control','required'=> true,'onblur'=>'javascript: formatarCampo(this);','onfocus'=>'javascript: retirarFormatacao(this);')))
-            ->add('coCpfCnpj', TextType::class, array('attr' => array('class' => 'form-control','required'=> true,'min'=>11,'max' => 14,'onblur'=>'javascript: formatarCampo(this);','onfocus'=>'javascript: retirarFormatacao(this);')))
-            ->add('dsNome', TextType::class, array('attr' => array('class' => 'form-control','required'=> true,'maxlength' => 100)))
-            ->add('atualizar', SubmitType::class, array('label' => 'Atualizar','attr' => array('class' => 'btn btn-primary mt-3')))
+            ->add(
+                'nuSeqEmpresa',
+                NumberType::class,
+                [
+                    'attr' =>
+                    [
+                        'class' => 'form-control',
+                        'required'=> true,
+                        'onblur'=>'javascript: formatarCampo(this);',
+                        'onfocus'=>'javascript: retirarFormatacao(this);'
+                    ]
+                ])
+            ->add(
+                'coCpfCnpj',
+                TextType::class,
+                [
+                    'attr' =>
+                    [
+                        'class' => 'form-control',
+                        'required'=> true,
+                        'max' => 14,
+                        'onblur'=>'javascript: formatarCampo(this);',
+                        'onfocus'=>'javascript: retirarFormatacao(this);'
+                    ]
+                ])
+            ->add(
+                'dsNome',
+                TextType::class,
+                [
+                    'attr' =>
+                    [
+                        'class' => 'form-control',
+                        'required'=> true,
+                        'maxlength' => 100
+                    ]
+                ])
+            ->add('atualizar',
+                SubmitType::class,
+                [
+                    'label' => 'Atualizar',
+                    'attr' => ['class' => 'btn btn-primary mt-3']
+                ])
             ->getForm();
-
             $form->handleRequest($request);
+
             if ($form->isSubmitted() && $form->isValid()) {
                 $entityManager = $this->getDoctrine()->getManager();
                 $entityManager->flush();
+
                 return $this->redirectToRoute('listarPessoa');
             }
+
             return $this->render('Pessoas/editarPessoas.html.twig', array('form' => $form->createView()));
         }
 
         /**
         * @Route("/pessoas/{nuSeqPessoa}", name="verPessoa")
         */
-        public function showPessoa($nuSeqPessoa) 
+        public function showPessoa($nuSeqPessoa)
         {
             $pessoa = $this->getDoctrine()->getRepository(Pessoas::class)->find($nuSeqPessoa);
             $empresa= $this->getDoctrine()->getRepository(Empresas::class)->find((int)$pessoa->getNuSeqEmpresa());
+
             return $this->render('Pessoas/verPessoas.html.twig', array('pessoa' => $pessoa,'empresa'=>$empresa));
         }
+
     }
-
-
-
