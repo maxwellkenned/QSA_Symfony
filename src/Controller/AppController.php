874352d13860ce6f827aeba2046b715d5567680d
AlexandreMatheus <alexandrematheuscg@hotmail.com>
2019-03-05 14:16:35 -0300

    'V1.1'

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,23 +1,22 @@
 <?php
 
-    namespace App\Controller;
+namespace App\Controller;
 
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\HttpFoundation\Request;
-    use Symfony\Component\Routing\Annotation\Route;
-    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-    class AppController extends Controller 
+class AppController extends Controller
+{
+    /**
+     * @Route("/" ,name="indexGeral")
+     * @Method({"GET"})
+     */
+    public function listarEmpresas()
     {
-        /**
-         * @Route("/" ,name="indexGeral")
-         * @Method({"GET"})
-         */
-        public function listarEmpresas()
-        {
             
-            return $this->render('inc/Index.html.twig');
-        }
-
+        return $this->render('inc/Index.html.twig');
     }
+}
