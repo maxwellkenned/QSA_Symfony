4de8a6bd16ddc1c472482d27caf7241c59517a40
Maxwell Kenned <kenned123@gmail.com>
2019-03-05 16:45:59 -0300

    Correções
    @AlexandreMatheus

--- src/Controller/EmpresasController.php
+++ src/Controller/EmpresasController.php
@@ -23,6 +23,7 @@ class EmpresasController extends Controller
      */
     public function listarEmpresas()
     {
+        /** @TODO Criar um servico para fazer isso EmpresaModelBusiness */
         $empresas = $this->getDoctrine()->getRepository(Empresas::class)->findAll();
         
         return $this->render('Empresas/IndexEmpresas.html.twig', array('empresas'=>$empresas));
@@ -35,42 +36,45 @@ class EmpresasController extends Controller
     public function criarEmpresa(Request $request)
     {
         $empresa = new Empresas();
+        /** @TODO Corrigir indentação */
         $form = $this->createFormBuilder($empresa)
         ->add(
             'coCnpj',
             TextType::class,
             [
-            'attr' =>
-            [
-                'class' => 'form-control',
-                'required'=> true,
-                'max' => 14,
-                'onblur'=>'javascript: formatarCampo(this);',
-                'onfocus'=>'javascript: retirarFormatacao(this);'
-            ]]
-        )
+                'attr' => [
+                    'class' => 'form-control',
+                    'required'=> true,
+                    'max' => 14,
+                    'onblur'=>'javascript: formatarCampo(this);',
+                    'onfocus'=>'javascript: retirarFormatacao(this);'
+                ]
+            ]
+         )
         ->add(
             'razaoSocial',
             TextType::class,
-            ['attr' =>
             [
-                'class' => 'form-control',
-                'required'=> true,
-                'maxlength' => 100
-            ]]
+                'attr' => [
+                    'class' => 'form-control',
+                    'required'=> true,
+                    'maxlength' => 100
+                ]
+            ]
         )
         ->add(
             'salvar',
             SubmitType::class,
             [
-            'label' => 'Salvar',
-            'attr' => ['class' => 'btn btn-primary mt-3']
+                'label' => 'Salvar',
+                'attr' => ['class' => 'btn btn-primary mt-3']
             ]
         )
         ->getForm();
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @TODO Criar função na classe EmpresaModelBusiness */
             $empresa = $form->getData();
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($empresa);
@@ -88,7 +92,9 @@ class EmpresasController extends Controller
      */
     public function baixarEmpresa(Request $request, $nuSeqEmpresa)
     {
+        /** @TODO Criar função find na classe EmpresaModelBusiness */
         $empresa = $this->getDoctrine()->getRepository(Empresas::class)->find($nuSeqEmpresa);
+        /** @TODO Criar função remover na classe EmpresaModelBusiness */
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->remove($empresa);
         $entityManager->flush();
@@ -104,39 +110,43 @@ class EmpresasController extends Controller
     */
     public function editarEmpresas(Request $request, $nuSeqEmpresa)
     {
+        /** @TODO Criar função find na classe EmpresaModelBusiness */
         $empresa = new Empresas();
         $empresa = $this->getDoctrine()->getRepository(Empresas::class)->find($nuSeqEmpresa);
+        /** @TODO Corrigir indentação */
         $form = $this->createFormBuilder($empresa)
         ->add(
             'coCnpj',
             TextType::class,
             [
-            'attr' =>
-            [
-                'class' => 'form-control',
-                'required'=> true,
-                'max' => 14,
-                'onblur'=>'javascript: formatarCampo(this);',
-                'onfocus'=>'javascript: retirarFormatacao(this);'
-            ]]
+                'attr' =>
+                    [
+                        'class' => 'form-control',
+                        'required'=> true,
+                        'max' => 14,
+                        'onblur'=>'javascript: formatarCampo(this);',
+                        'onfocus'=>'javascript: retirarFormatacao(this);'
+                    ]
+            ]
         )
         ->add(
             'razaoSocial',
             TextType::class,
             [
-            'attr' =>
-            [
-                'class' => 'form-control',
-                'required'=> true,
-                'maxlength' => 100
-            ]]
+                'attr' =>
+                    [
+                        'class' => 'form-control',
+                        'required'=> true,
+                        'maxlength' => 100
+                    ]
+            ]
         )
         ->add(
             'atualizar',
             SubmitType::class,
             [
-            'label' => 'Atualizar',
-            'attr' => ['class' => 'btn btn-primary mt-3']
+                'label' => 'Atualizar',
+                'attr' => ['class' => 'btn btn-primary mt-3']
             ]
         )
         ->getForm();
@@ -144,6 +154,7 @@ class EmpresasController extends Controller
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @TODO Criar função de atualizar na classe EmpresaModelBusiness */
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->flush();
             
@@ -158,6 +169,7 @@ class EmpresasController extends Controller
     */
     public function verEmpresa($nuSeqEmpresa)
     {
+        /** @TODO Criar função find na classe EmpresaModelBusiness */
         $empresa = $this->getDoctrine()->getRepository(Empresas::class)->find($nuSeqEmpresa);
         $pessoas= $this->getDoctrine()->getRepository(Pessoas::class)->findBy(
             ['nuSeqEmpresa' => (int)$empresa->getNuSeqEmpresa()]
